71e4439c347d9f624e7bc7a55e7b2297b05bd09f
lucasdemartha <lucasribm@gmail.com>
2023-08-31 14:31:40 -0300

    att

--- Aula03/Ex4.py
+++ Aula03/Ex4.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-mtz1 = np.ones(12).reshape(3,4)
+mtz1 = np.ones((3, 4), dtype=int)
 print("Matriz 3x4: \n", mtz1)
-mtz2 = mtz1.ravel()
+mtz2 = mtz1.ravel() #ou mtz1.flatten()
 print("Matrix 1-D: \n", mtz2)
--- Aula03/Ex5.py
+++ Aula03/Ex5.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+'''
 matriz = np.random.randint(0, 10, (5, 6))
 
 linhas = matriz.shape[0]
@@ -11,3 +11,26 @@ if total_elementos % 2 == 0:
   print("A matriz pode se tornar um vetor com numero par de elementos")
 else:
   print("A matriz pode se tornar um vetor com numero ímpar de elementos")
+'''
+
+# Criar uma matriz 4x5 de exemplo
+matriz = np.array([[1, 2, 3, 4, 5],
+                   [6, 7, 8, 9, 10],
+                   [11, 12, 13, 14, 15],
+                   [16, 17, 18, 19, 20]])
+
+# Obter número de linhas e colunas
+num_linhas, num_colunas = matriz.shape
+
+# Calcular o produto entre o número de linhas e colunas
+produto = num_linhas * num_colunas
+
+# Verificar se o produto é par ou ímpar
+resultado = "par" if produto % 2 == 0 else "ímpar"
+
+print("Matriz:")
+print(matriz)
+print(f"Numero de linhas: {num_linhas}")
+print(f"Numero de colunas: {num_colunas}")
+print(f"Produto (linhas x colunas): {produto}")
+print(f"A matriz poderia se tornar um vetor com um numero {resultado} de elementos.")
